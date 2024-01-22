8b8b09cc72b6cd608bc3aed9850b5f3943a41eaa
KaitodotWav <78613734+KaitodotWav@users.noreply.github.com>
2024-01-23 01:48:19 +0800

    Temporary Update HSelector.java

--- MenuProgram/HSelector.java
+++ MenuProgram/HSelector.java
@@ -65,9 +65,17 @@ public class HSelector extends Frame implements ActionListener {
                 int ent4 = Integer.parseInt(entry4.getText());
                 int ent5 = Integer.parseInt(entry5.getText());
                 String highest = "1st number is the highest : "+ent1;
-                if (ent2 >= 1){
-                    
-                }
+                if (num1<=num2 & num1<=num3 & num1<=num4 & num1<=num5){
+            System.out.println(num1+" is the lowest!");
+        } else if (num2<=num1 & num2<=num3 & num2<=num4 & num2<=num5){
+            System.out.println(num2+" is the lowest!");
+        } else if (num3<=num2 & num3<=num1 & num3<=num4 & num3<=num5){
+            System.out.println(num3+" is the lowest!");
+        } else if (num4<=num2 & num4<=num3 & num4<=num1 & num4<=num5){
+            System.out.println(num4+" is the lowest!");
+        } else {
+            System.out.println(num5+" is the lowest!");
+        }
             } catch (Exception ex){
             }
         }
